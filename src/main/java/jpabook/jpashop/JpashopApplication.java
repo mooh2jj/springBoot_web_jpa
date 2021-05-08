6a6f636df2e347455e53ead8c66fc0b7dec0dcd2
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-05-08 23:55:39 +0900

    타임리프 설정

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -8,12 +8,7 @@ public class JpashopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpashopApplication.class, args);
-        System.out.println("hello jpabook!!");
 
-        Hello hello = new Hello();
-        hello.setData("hello");
-        String data = hello.getData();
-        System.out.println("data:"+data);
     }
 
 }
