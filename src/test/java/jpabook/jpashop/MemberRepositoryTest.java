da6cbfd3a480ac8ec93d1d3a9680cbbc9a2b4d98
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-03-31 15:39:29 +0900

    주석으로 설명 추가

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -15,14 +15,18 @@ class MemberRepositoryTest {
     private MemberRepository memberRepository;
 
     @Test
-    @Transactional
-    @Rollback(value = false)
+    @Transactional      // 이거 안하면 오류남!! entityManager로 하는 건 꼭 @Transactional 이 필요!
+    @Rollback(value = false)        // test 끝나면 기본적으로 rollback을 하기 때문에 false 설정을 해야 db에서 확인할 수 있다!!
     public void testMember() throws Exception{
+        // given
         Member member = new Member();
         member.setUsername("memberA");
+
+        // when
         Long savedId = memberRepository.save(member);
         Member findMember = memberRepository.find(savedId);
 
+        // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member);
